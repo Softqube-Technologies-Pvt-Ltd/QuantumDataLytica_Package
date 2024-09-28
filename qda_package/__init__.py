e4aa156e6dfcaf1073005efd7c3f9a834780c5a1
Hardik <hardik.kanak@softqubes.com>
2024-09-28 17:52:06 +0530

    version 1.0.1

--- qda_package/__init__.py
+++ qda_package/__init__.py
@@ -1,3 +1,3 @@
 # my_interface/__init__.py
 # from .parent_interface import ParentInterface
-from .ParentMachineInterface import ParentMachineInterface
+from .QDMachineInterface import QDMachineInterface 
