64481a927c4544328b34b1f707337da371f18caa
Hardik <hardik.kanak@softqubes.com>
2024-09-28 18:35:44 +0530

    version 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qda_package",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     include_package_data=True,  # Include all data files (important for including pytransform)
     package_data={
@@ -11,7 +11,8 @@ setup(
     install_requires=[  # List of dependencies
         "requests==2.31.0",
         "psutil==6.0.0",
-        "GPUtil==1.4.0"
+        "GPUtil==1.4.0",
+        "boto3==1.34.4"
     ],
     description="Helping Package for QuantumDataLytica Machine",
     author="Switchboard LLC",
