79f3d2a7663d6c8ad918d32cdb611aa6a40eb2a4
Hardik <hardik.kanak@softqubes.com>
2024-09-28 21:07:13 +0530

    version="1.0.7"

--- qda_package/__init__.py
+++ qda_package/__init__.py
@@ -1,3 +1,1 @@
-# my_interface/__init__.py
-# from .parent_interface import ParentInterface
 from .QDMachineInterface import QDMachineInterface 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qda_package",
-    version="1.0.6",
+    version="1.0.7",
     packages=find_packages(),
     include_package_data=True,  # Include all data files (important for including pytransform)
     package_data={
