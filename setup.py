0dae43eaaf5c09db78cfdc2fab0d10fb66173e92
Hardik <hardik.kanak@softqubes.com>
2024-10-05 21:08:09 +0530

    version="1.1.1"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="qda_package",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     include_package_data=True,  # Include all data files (important for including pytransform)
     package_data={
